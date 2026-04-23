2c815406a55a18c1afd1f78836d2398a356d6bf8
Diogo Iwasaki <67493856+Iwaslazkis@users.noreply.github.com>
2026-04-23 03:31:01 -0400

    Make Telescope oldfiles include current session

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,10 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>fo", function()
+  require("telescope.builtin").oldfiles {
+    include_current_session = true,
+  }
+end, { desc = "telescope find oldfiles" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
